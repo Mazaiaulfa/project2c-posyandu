1968e85ce4e9e965cc19d55d483e190bf2705490
Mazaiaulfa <113304807+Mazaiaulfa@users.noreply.github.com>
2023-06-14 13:55:27 +0700

    commit baru

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,11 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('login');
 });
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return view('dashboard');
 });
 
